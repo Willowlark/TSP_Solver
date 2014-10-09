054c0ba3625b4b7685e685531ee22d853ecfe754
Bill Clark <clarkw26@students.rowan.edu>
2014-10-08 20:13:46 -0400

    hot damn almost forgot the comments

--- nearneigh.py
+++ nearneigh.py
@@ -3,13 +3,15 @@ from sys import maxint
 import copy
 import time
 
-#Global vars.
+#Global variables.
 citycount = 0
 cities = {}
 shortestdistance = maxint
 shortestpath = []
 start = 0
 
+#Calculates the distance between two cities, stored as an
+#ordered pair list.
 def distance_math(one, two):
 	x = sqrt((two[0] - one[0])**2 + (two[1] - one[1])**2)
 	return x
@@ -17,6 +19,7 @@ def distance_math(one, two):
 #Asks for a file, then reads in the relevant information.
 f = raw_input("enter filename: ")
 
+#starts the clock on the calculation time.
 start = time.time()
 
 file = open(f, 'r')
@@ -25,25 +28,33 @@ for line in file:
 	if words[0] == 'DIMENSION:':
 			citycount = int(words[1])
 	if words[0].isdigit():
-		x = [int(float(words[1])), int(float(words[2]))]
+		x = [float(words[1]), float(words[2])]
 		cities[int(words[0])] = x
 	else:
 		pass
 
+#Main Operations Section. For each possible starting node, it takes the other nodes and
+#sorts them in comparision with the last node visited. It takes the minimum and appends it
+#to the visited list, until it loops back to node 1.
+
+#Print comments from testing left in as they make quite a readable printout, if you want
+#to see the paths as they are calcuated. 
 for starter in [x+1 for x in range(citycount)]:
-	unvisited = cities.copy()
+	unvisited = copy.deepcopy(cities)
 	visited = [starter]
 	del unvisited[starter]
 	totaldistance = 0
-	
+	#print starter, cities[visited[-1]]
 	while unvisited:
-		city = min(unvisited.keys(), key=lambda c: distance_math(cities[visited[-1]], cities[c]))
-		totaldistance += distance_math(cities[visited[-1]], cities[city])
+		#print unvisited
+		city = min(unvisited.keys(), key=lambda c: distance_math(cities[c], cities[visited[-1]]))
+		totaldistance = totaldistance + distance_math(cities[visited[-1]], cities[city])
 		visited.append(city)
 		del unvisited[city]
-	totaldistance += distance_math(cities[visited[-1]], cities[starter])
+		
+	totaldistance = totaldistance + distance_math(cities[visited[-1]], cities[starter])
 	visited.append(starter)
-	
+	#print totaldistance
 	if shortestdistance > totaldistance:
 		shortestdistance = totaldistance
 		shortestpath = copy.deepcopy(visited)
